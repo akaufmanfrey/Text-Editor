c231ead089dcf08425388beea6f006f28473c44d
akaufm <akaufmanfrey@gmail.com>
2024-08-26 10:27:28 -0400

    added webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,64 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "JATE",
+        favicon: "./favicon.ico",
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "A tool for taking notes and saving them online or offline.",
+        favicon: "./favicon.ico",
+        // Theme and background color pulled from duotone-light.min.css
+        background_color: "#faf8f5",
+        theme_color: "#b29762",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        // CSS loader
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        // Image loader
+        // {
+        //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        //   type: "asset/resource",
+        // },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // Babel-loader in order to use ES6.
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
